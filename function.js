de6713695b8d65a493bfc286d3cab85e6b28126e
ManuelBenitez01 <josemanuelbenitez01@gmail.com>
2025-01-24 07:37:20 -0300

    commit 3

--- function.js
+++ function.js
@@ -1,19 +1,18 @@
-let btnrates = document.querySelector(".btnrates");
-let caracteristicas = document.querySelector(".caracteristicas");
-let btnrates2 = document.querySelector(".btnrates2");
-let general = document.querySelector(".general");
-let btnrates3 = document.querySelector(".btnrates3");
+let btnrates = document.querySelectorAll(".btnrates");
+let element = document.querySelectorAll(".element");
 
-let btnrates4 = document.querySelector(".btnrates4");
-let btnrates5 = document.querySelector(".btnrates5");
-let btnrates6 = document.querySelector(".btnrates6");
 
- 
-btnrates.addEventListener("click", ()=> {
-     caracteristicas.classList.toggle("hidden");
-});
-btnrates2.addEventListener("click", ()=> { 
-    general.classList.toggle("hidden");
-});
+btnrates.forEach((btn, i) => {
+    btn.addEventListener("click", ()=> {
+         element[i].classList.toggle("none");
+         
+         for (let j = 0; j < element.length; j++) {
+            if (j != i) {
+                element[j].classList.add("none");
+            }
+        }
+    })
+})
+
 
 
